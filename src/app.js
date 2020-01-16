2316f3afd11501d491d0f286b00ff963e1b78181
akshayks3 <akshayks3573@gmail.com>
2020-01-16 13:06:35 +0530

    ariyoola

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.get("", (req, res) => {
+  res.send("Hello express!");
+});
+app.get("/help", (req, res) => {
+  res.send("Help Page");
+});
+app.get("/about",(req,res)=>{
+  res.send("About page")
+})
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
